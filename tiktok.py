aea496a33d7f0d73d6f68cafe5321fd946e4e5e8
Kopral <68215949+kopralgosong@users.noreply.github.com>
2021-10-06 13:29:15 +0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -5,13 +5,11 @@ def palupila(kode,urut):
         url = 'http://api.areltiyan.xyz/tiktokwm?link='+kode
         r = requests.get(url,verify=False).json()
         print (r['status'])
-        a = (r['no_watermark_link'])
-        file_name = 'oke.mp4'
-        wget.download(a,file_name)
-        print ('\33[32m Success '+str(urut)+' \33[39m')
+        
     except:
         print('Gagal= ',kode)
         pass
+    return r
 print ('Tiktok Mass Download Thx to : \33[96mArel Tiyan\33[39m ')
 list_kode = input("List url tiktoknya ngab : ")
 read_kode = open(list_kode,"r") 
@@ -20,3 +18,7 @@ for kode in read_kode:
     urut +=1
     kode = kode.strip()
     palupila(kode,urut)
+    a = (r['no_watermark_link'])
+    file_name = 'oke.mp4'
+    wget.download(a,file_name)
+    print ('\33[32m Success '+str(urut)+' \33[39m')
